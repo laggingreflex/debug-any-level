79e45ee9b0a4e52895dbf975a6e52b09b643e15d
laggingreflex <laggingreflex@gmail.com>
2017-07-17 05:05:28 +0530

    fix: "debug" property
    return logger instead of "debug" (main module) when property key "debug" is used.
    
    factorize common "get"

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const debug = require('debug');
 
 const cache = {};
 
-const debugAny = new Proxy((name, ...rest) => new Proxy(debug(name, ...rest), {
-  get: (t, k, r) =>
-    Reflect.get(t, k, r)
-    || Reflect.get(debug, k, r)
-    || cache[`${name}:${k}`]
-    || (cache[`${name}:${k}`] = debugAny(`${name}:${k}`, ...rest))
-}), {
-  get: (t, k, r) =>
-    Reflect.get(t, k, r)
-    || Reflect.get(debug, k, r)
-    || cache[k]
-    || (cache[k] = debugAny(k))
-});
+function get(t, k, r, name) {
+  const defProp = Reflect.get(t, k, r) || Reflect.get(debug, k, r);
+  if (defProp && defProp !== debug) {
+    return defProp;
+  } else {
+    if (name) {
+      k = `${name}:${k}`;
+    }
+    return cache[k] || (cache[k] = debugAny(k));
+  }
+}
+
+const debugAny = new Proxy((name, ...rest) => new Proxy(debug(name, ...rest), { get: (...args) => get(...args, name) }), { get });
 
 module.exports = debugAny;
